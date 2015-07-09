83a8830abf7707c72f75a76936b89f166a6ea3ad
Laurence Elsdon <git@elsdon.me>
2015-07-09 23:53:42 +0100

    Revisions

--- app/app.js
+++ app/app.js
@@ -1,12 +1,32 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
+/*****************************************************************
+*
+* Declare app level module which depends on views, and components
+*
+******************************************************************/
+angular.module('app', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  'LocalStorageModule',
+  'navList',
+  'angularFileUpload',
+  'app.config',
+  'app.dashboard',
+  'app.pages'
+])
+
+/*****************************************************************
+*
+* Route provider
+*
+******************************************************************/
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/404'});
+}])
+
+/*****************************************************************
+*
+* Lodash
+*
+******************************************************************/
+.constant('_', window._)
